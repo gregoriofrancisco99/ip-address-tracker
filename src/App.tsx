8be24d4daae67efa972928da140ea4d7650e09b3
Gregório Francisco <gregoriofrancisco2014@gmail.com>
2022-09-17 21:32:39 +0100

    Creating project, adding map

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
-function App() {
+import '../src/styles/app.scss'
+import 'leaflet/dist/leaflet.css';
+
+import arrowIcon from '../src/assets/images/icon-arrow.svg';
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header>
+        <h1>
+          IP Address Tracker
+        </h1>
+        <form>
+          <input placeholder="192.168.1.1" />
+          <button type="submit">
+            <img src={arrowIcon} alt="" />
+          </button>
+        </form>
       </header>
+      <main>
+      <div className="table">
+        <div className="column first">
+            <h3>IP Address</h3>
+            <span>
+              <strong>192.212.174.101</strong>
+            </span>
+          </div>
+          <div className="column second">
+            <h3>Location</h3>
+            <span>
+              <strong>Brooklyn, NY <br /> 10001</strong>
+            </span>
+          </div>
+          <div className="column three">
+            <h3>TimeZone</h3>
+            <span>
+              <strong>UTC-05:00</strong>
+            </span>
+          </div>
+          <div className="column four">
+            <h3>ISP</h3>
+            <span>
+              <strong>SpaceX <br /> Starlink</strong>
+            </span>
+          </div>
+        </div>
+      
+        <MapContainer 
+          center={[-12.3592333, 13.5964385]}
+          zoom={14.19}
+          scrollWheelZoom={true}
+          style={{width: '100%', height: '100%'}}
+        > 
+          {/*url=`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`*/}
+          <TileLayer
+            url= {'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'}
+          />
+        </MapContainer>
+
+      </main>
     </div>
   );
 }
 
-export default App;
