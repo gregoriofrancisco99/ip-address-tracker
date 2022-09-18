9d454e95b6efd754bd800a336fb295700cf7152e
Gregório Francisco <gregoriofrancisco2014@gmail.com>
2022-09-18 03:43:34 +0100

    IP tracker function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
+import { FormEvent } from 'react';
+import { apikey } from './services/geolocation';
+
 
 import '../src/styles/app.scss'
 import 'leaflet/dist/leaflet.css';
 
 import arrowIcon from '../src/assets/images/icon-arrow.svg';
+import { useEffect, useState } from 'react';
+
+type IPInfoType = {
+  ip: string,
+  isp:string,
+  location: {
+    country: string,
+    region: string,
+    timezone: string
+  }
+}
 
 export function App() {
-  return (
+  const [ipInfo, setIpInfo] = useState<IPInfoType>();
+  const [IP, setIP] = useState('');
+
+  async function AskAPI(form:FormEvent) {
+    form.preventDefault();
+
+    const url = `https://geo.ipify.org/api/v2/country?apiKey=${apikey}&ipAddress=${IP}`
+    
+    await fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        setIpInfo(result);
+        setIP('')
+        console.log(ipInfo);
+    });
+  }
+
+ 
+ return (
     <div className='app'>
       <header>
         <h1>
           IP Address Tracker
         </h1>
-        <form>
-          <input placeholder="Search for any IP address or domain" />
+        <form onSubmit={AskAPI}>
+          <input 
+            placeholder="Search for any IP address or domain" 
+            onChange={(newIpValue) => {setIP(newIpValue.target.value);}}
+            value={IP}
+          />  
           <button type="submit">
             <img src={arrowIcon} alt="" />
           </button>
@@ -24,25 +60,25 @@ export function App() {
         <div className="column first">
             <h3>IP Address</h3>
             <span>
-              <strong>192.212.174.101</strong>
+              <strong>{ (ipInfo?.ip) ? ipInfo?.ip : '---'}</strong>
             </span>
           </div>
           <div className="column second">
             <h3>Location</h3>
             <span>
-              <strong>Brooklyn, NY <br /> 10001</strong>
+              <strong>{ (ipInfo?.location.region || ipInfo?.location.country) ? `${ipInfo.location.region}, ${ipInfo.location.country} `: '---'}</strong>
             </span>
           </div>
           <div className="column three">
             <h3>TimeZone</h3>
             <span>
-              <strong>UTC-05:00</strong>
+              <strong>{ (ipInfo?.location.timezone) ? ipInfo?.location.timezone : '---'}</strong>
             </span>
           </div>
           <div className="column four">
             <h3>ISP</h3>
             <span>
-              <strong>SpaceX <br /> Starlink</strong>
+              <strong>{ (ipInfo?.isp) ? ipInfo.isp : '---'}</strong>
             </span>
           </div>
         </div>
@@ -63,4 +99,3 @@ export function App() {
     </div>
   );
 }
-
